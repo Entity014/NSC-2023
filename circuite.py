1d8c9383b7e9d99d2a220a813aa00f72f95f3ffb
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2022-12-21 13:31:59 +0700

    Add files via upload

---
+++ circuite.py
@@ -0,0 +1,8 @@
+import schemdraw
+import schemdraw.elements as elm
+with schemdraw.Drawing(file='schematic.svg') as d:
+    d += elm.Resistor().label('100KΩ')
+    d += elm.Capacitor().down().label('0.1μF', loc='bottom')
+    d += elm.Line().left()
+    d += elm.Ground()
+    d += elm.SourceV().up().label('10V')

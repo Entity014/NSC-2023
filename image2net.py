4d16c5528cc40b64a177f126e387f0d3a2841705
Entity014 <radlove.012@gmail.com>
2023-02-23 18:27:33 +0700

    เหลือคัดว่า pin อยู่กับ R ไหน

--- image2net.py
+++ image2net.py
@@ -1,4 +1,5 @@
-f = open("NSC-2023/runs/detect/exp3/labels/1.txt", "r")
+import numpy as np
+f = open("1.txt", "r")
 arr = []
 for i, x in enumerate(f):
     arr.append([float(i) for i in x.split(' ')])
@@ -7,13 +8,50 @@ for i in range(len(arr)):
     for j in range(1, len(arr[i])):
         arr[i][j] = arr[i][j] * 100
 
-pinP = []
+pinP = [] # index 5 is order
 objR = []
-for i, x in enumerate(arr):
+pinXY = []
+objXY = []
+for x in arr:
     if (x[0] == 4):
         pinP.append(x)
+        pinXY.append([x[1], x[2]])
     else:
         objR.append(x)
-
-for i in range(len(pinP)):
+        objXY.append([x[1], x[2]])
     
+pinXY = np.array(pinXY)
+objXY = np.array(objXY)
+indP = np.lexsort((pinXY[:,1],pinXY[:,0]))   
+indO = np.lexsort((objXY[:,1],objXY[:,0]))
+
+for i, x in enumerate(pinP):
+    for j, y in enumerate(pinXY[indP]):
+        if (y[0] in x) and (y[1] in x):
+            x.append(j)
+    # print(x)
+
+for i, a in enumerate(objR):
+    for j, b in enumerate(objXY[indO]):
+        if (b[0] in a) and (b[1] in a):
+            a.append(j)
+    # print(a)
+# print(pinP[4])
+# print(pinP[2])
+# print(objR[1])
+
+# 1 > 2, 3
+# 0 > 0, 1
+# 2 > 4
+# * 10%
+node = []
+dummy = []
+for i, x in enumerate(objR):
+    for j, y in enumerate(pinP):
+        if ((x[2] - (x[2] * 0.1)) < y[2]) and (x[2] > y[2]):
+            node.append([x[5], y[5]])
+            # print(y[2], y[5], x[5])
+        if ((x[2] + (x[2] * 0.1)) > y[2]) and (x[2] < y[2]):
+            node.append([x[5], y[5]])
+            # print(y[2], y[5], x[5])
+    print(node)

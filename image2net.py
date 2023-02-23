3e01a5722cf05b3eaefa4b9b923ac3edff16135a
Entity014 <radlove.012@gmail.com>
2023-02-24 00:49:04 +0700

    เสร็จแล้ว >~<

--- image2net.py
+++ image2net.py
@@ -28,29 +28,37 @@ indO = np.lexsort((objXY[:,1],objXY[:,0]))
 for i, x in enumerate(pinP):
     for j, y in enumerate(pinXY[indP]):
         if (y[0] in x) and (y[1] in x):
-            x.append(j)
-    # print(x)
+            x.append("P%s" % j)
+    print(x)
 
 for i, a in enumerate(objR):
     for j, b in enumerate(objXY[indO]):
         if (b[0] in a) and (b[1] in a):
-            a.append(j)
-    # print(a)
-# print(pinP[4])
-# print(pinP[2])
-# print(objR[1])
+            a.append("R%s" % j)
+    print(a)
 
 # 1 > 2, 3
 # 0 > 0, 1
 # 2 > 4
 # * 10%
 node = [] # [R ตัวไหน, คาดว่ามี Pin ไหน]
+netlist = []
 for i, x in enumerate(objR):
     for j, y in enumerate(pinP):
-        if ((x[2] - (x[2] * 0.1)) < y[2]) and (x[2] > y[2]):
+        if ((x[1] - (x[1] * 0.1)) < y[1]) and (x[1] > y[1]):
             node.append([x[5], y[5]])
-            # print(y[2], y[5], x[5])
-        if ((x[2] + (x[2] * 0.1)) > y[2]) and (x[2] < y[2]):
+            # print(y[1], y[5], x[5])
+        if ((x[1] + (x[1] * 0.1)) > y[1]) and (x[1] < y[1]):
             node.append([x[5], y[5]])
-            # print(y[2], y[5], x[5])
-    print(node)
+            # print(y[1], y[5], x[5])
+print(node)
+for i, x in enumerate(objR):
+    for j, y in enumerate(pinP):
+        for k, z in enumerate(node):
+            if (z[0] in x and z[1] in y):
+                if ((x[2] + x[4]) > y[2]) and (x[2] < y[2]):
+                    netlist.append([x[5], y[5]])
+
+print(netlist)
+# if ((objR[0][2] + objR[0][4]) > pinP[0][2]) and (objR[0][2] < pinP[0][2]):
+#     print()

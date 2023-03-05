fb196d89df23e99d075380e95688837a6f8b1761
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2023-03-06 01:23:21 +0700

    check error

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import networkx as nx
 from lcapy import Circuit, Vstep, R, C
 import matplotlib.pyplot as plt
 import numpy as np
+from image2net import toNetlist
 
 def main():
+    ready, netlist = toNetlist()
     G = nx.Graph()
-    netlist = [[1, "R1", 1, 2], [2, "R2", 1, 3], [3, "R3", 3, 4], [4, "C4", 5, 4], [5, "C5", 6, 5], [6, "D6", 2, 6]]
+    # netlist = [[1, 'R0', 1, 999], [2, 'R1', 1, 2], [3, 'R2', 2, 3]]
+    # netlist = [[1, "R1", 1, 2], [2, "R2", 1, 3], [3, "R3", 3, 4], [4, "C4", 5, 4], [5, "C5", 6, 5], [6, "D6", 2, 6]]
     posi = np.zeros((len(netlist), 2))
     for i, arr in enumerate(netlist):
         G.add_node(arr[0], label=f"{arr[0]}")
@@ -15,28 +18,31 @@ def main():
     pos = nx.spring_layout(G)
     options = {'pos':pos, 'node_color': 'orange', 'node_size': 300, 'width': 3, 'labels': labels, 'font_weight': 'bold'}
     nx.draw(G, **options)
+    if not ready:
+        print("Error")
     # post = net(G, netlist)
-    array = []
-    for i, j in enumerate(G.edges):
-        for x, y in enumerate(G.nodes):
-            if ((j[0] == netlist[x][2]) and (j[1] == netlist[x][3])) or ((j[0] == netlist[x][3]) and (j[1] == netlist[x][2])):
-                array.append(y - 1)
-    # print(array)
-    # print(G.edges, G.nodes)
-    for i in range(1, len(pos) + 1):
-        posi[i - 1] = [pos[i][0], pos[i][1]]
-    cct = Circuit()
-    for i, j in enumerate(G.edges):
-        if ((posi[j[0] -1][1] - posi[j[1] - 1][1]) * 100) >= 70:
-            cct.add(f'{netlist[array[i]][1]} {j[0]} {j[1]}; down')
-        elif (((posi[j[0] -1][1] - posi[j[1] - 1][1]) * 100) > 0) or (((posi[j[0] -1][1] - posi[j[1] -1][1]) * 100) > -70):
-            if ((posi[j[0] -1][0] - posi[j[1] - 1][0]) * 100) > 70:
-                cct.add(f'{netlist[array[i]][1]} {j[0]} {j[1]}; left')
+    if ready:
+        array = []
+        for i, j in enumerate(G.edges):
+            for x, y in enumerate(G.nodes):
+                if ((j[0] == netlist[x][2]) and (j[1] == netlist[x][3])) or ((j[0] == netlist[x][3]) and (j[1] == netlist[x][2])):
+                    array.append(y - 1)
+        # print(array)
+        # print(G.edges, G.nodes)
+        for i in range(1, len(pos) + 1):
+            posi[i - 1] = [pos[i][0], pos[i][1]]
+        cct = Circuit()
+        for i, j in enumerate(G.edges):
+            if ((posi[j[0] -1][1] - posi[j[1] - 1][1]) * 100) >= 70:
+                cct.add(f'{netlist[array[i]][1]} {j[0]} {j[1]}; down')
+            elif (((posi[j[0] -1][1] - posi[j[1] - 1][1]) * 100) > 0) or (((posi[j[0] -1][1] - posi[j[1] -1][1]) * 100) > -70):
+                if ((posi[j[0] -1][0] - posi[j[1] - 1][0]) * 100) > 70:
+                    cct.add(f'{netlist[array[i]][1]} {j[0]} {j[1]}; left')
+                else:
+                    cct.add(f'{netlist[array[i]][1]} {j[0]} {j[1]}; right')
             else:
-                cct.add(f'{netlist[array[i]][1]} {j[0]} {j[1]}; right')
-        else:
-            cct.add(f'{netlist[array[i]][1]} {j[0]} {j[1]}; up')
-    cct.draw()
+                cct.add(f'{netlist[array[i]][1]} {j[0]} {j[1]}; up')
+        cct.draw()
     plt.show()
 
 
